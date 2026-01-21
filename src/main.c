5ab37d76dfa78aa5400d6a235bd3a9b7578a14c5
Dan Shimmyo <dshimmyo@gmail.com>
2026-01-21 13:18:59 -0500

    add second box

--- src/main.c
+++ src/main.c
@@ -2,14 +2,12 @@
 #include "gt/gfx/draw_queue.h"
 
 char box_x = 30, box_y = 20;
-char dx = 1, dy = 1;
+char boxA_x = 20, boxA_y = 30;
 
-void main () {
-    while (1) {                                     //  Run forever
-        queue_clear_screen(3);
-        queue_draw_box(box_x, box_y, 8, 8, 92);
-        queue_clear_border(0);
-        
+char dx = 1, dy = 1;
+char dxA = -1, dyA = 1;
+
+void boxMotion(){
         box_x += dx;
         box_y += dy;
         if(box_x == 1) {
@@ -22,7 +20,56 @@ void main () {
         } else if(box_y == 112) {
             dy = -1;
         }
- 
+}
+void boxAMotion(){
+        boxA_x += dxA;
+        boxA_y += dyA;
+        if(boxA_x == 1) {
+            dxA = 1;
+        } else if(boxA_x == 119) {
+            dxA = -1;
+        }
+        if(boxA_y == 8) {
+            dyA = 1;
+        } else if(boxA_y == 112) {
+            dyA = -1;
+        }
+}
+void main () {
+    while (1) {                                     //  Run forever
+        queue_clear_screen(3);
+        queue_draw_box(box_x, box_y, 8, 8, 92);
+        queue_draw_box(boxA_x, boxA_y, 8, 8, 127);
+
+        queue_clear_border(0);
+        boxMotion();
+        boxAMotion();
+        // box_x += dx;
+        // box_y += dy;
+        // if(box_x == 1) {
+        //     dx = 1;
+        // } else if(box_x == 119) {
+        //     dx = -1;
+        // }
+        // if(box_y == 8) {
+        //     dy = 1;
+        // } else if(box_y == 112) {
+        //     dy = -1;
+        // }
+
+
+        // boxA_x += dxA;
+        // boxA_y += dyA;
+        // if(boxA_x == 1) {
+        //     dxA = 1;
+        // } else if(boxA_x == 119) {
+        //     dxA = -1;
+        // }
+        // if(boxA_y == 8) {
+        //     dyA = 1;
+        // } else if(boxA_y == 112) {
+        //     dyA = -1;
+        // }
         await_draw_queue();
         await_vsync(1);
         flip_pages();
