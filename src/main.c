fc9f963aa4d4dadec79f48f1730948eff194220f
Dan Shimmyo <dshimmyo@gmail.com>
2026-01-21 23:17:16 -0500

    added edge collision sounds

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include "gt/gametank.h"
 #include "gt/gfx/draw_queue.h"
+#include "gen/assets/audio.h"
+#include "gt/audio/music.h"
+// #define FEEDBACK_AMT 0x04
+// #define PITCH_MSB 0x10
+// #define PITCH_LSB 0x20
+// #define AMPLITUDE 0x30
 
 char box_x = 30, box_y = 20;
 char boxA_x = 20, boxA_y = 30;
@@ -9,7 +15,10 @@ char bgColor = 0;
 
 char dx = 1, dy = 1;
 char dxA = -2, dyA = 2;
-
+void soundTest(){
+    //play_sound_effect(ASSET__audio__hit_bin_ID,1);
+    play_sound_effect(ASSET__audio__flong_sfx_ID,(char)1);
+}
 void boxMotion(){
         //boxSkipFrame = !boxSkipFrame;//simulating half-speed motion
         boxSkipCount++;
@@ -20,31 +29,45 @@ void boxMotion(){
             box_y += dy;
             if(box_x == 1) {
                 dx = 1;
+                soundTest();
             } else if(box_x == 119) {
                 dx = -1;
+                soundTest();
             }
             if(box_y == 8) {
                 dy = 1;
+                soundTest();
+
             } else if(box_y == 112) {
                 dy = -1;
+                soundTest();
             }
         }
 }
+
 void boxAMotion(){
         boxA_x += dxA;
         boxA_y += dyA;
         if(boxA_x <= 1) {
             dxA = 2;
+            soundTest();
+
         } else if(boxA_x >= 119) {
             dxA = -2;
+            soundTest();
         }
         if(boxA_y <= 8) {
             dyA = 2;
+            soundTest();
         } else if(boxA_y >= 112) {
             dyA = -2;
+            soundTest();
         }
 }
 void main () {
+    init_music();
+    //play_sound_effect(ASSET__audio__hit_bin);
+
     while (1) {                                     //  Run forever
         queue_clear_screen(256);//256 black
         queue_draw_box(box_x, box_y, 8, 8, 92);
@@ -53,35 +76,10 @@ void main () {
         queue_clear_border(2);
         boxMotion();
         boxAMotion();
-        // box_x += dx;
-        // box_y += dy;
-        // if(box_x == 1) {
-        //     dx = 1;
-        // } else if(box_x == 119) {
-        //     dx = -1;
-        // }
-        // if(box_y == 8) {
-        //     dy = 1;
-        // } else if(box_y == 112) {
-        //     dy = -1;
-        // }
 
-
-        // boxA_x += dxA;
-        // boxA_y += dyA;
-        // if(boxA_x == 1) {
-        //     dxA = 1;
-        // } else if(boxA_x == 119) {
-        //     dxA = -1;
-        // }
-        // if(boxA_y == 8) {
-        //     dyA = 1;
-        // } else if(boxA_y == 112) {
-        //     dyA = -1;
-        // }
         await_draw_queue();
         await_vsync(1);
         flip_pages();
- 
+        tick_music();
     }
 }
