1a34189f00f10f1a18320df8998eb7f93e33bda9
Daniel Shimmyo <dshimmyo@gmail.com>
2026-01-27 15:04:26 -0500

    paddle added

--- src/main.c
+++ src/main.c
@@ -9,7 +9,11 @@
 // #define AMPLITUDE 0x30
 #define BOXCOLOR 92
 #define BOXCOLORA 127
-
+#define PADDLECOLOR 7
+#define PADDLEWIDTH 12
+#define PADDLEHEIGHT 4
+#define PADDLEY 110
+char halfPaddleWidth = PADDLEWIDTH/2;
 //#define INPUT_MASK_ALL_KEYS (INPUT_MASK_UP|INPUT_MASK_DOWN|INPUT_MASK_LEFT|INPUT_MASK_RIGHT|INPUT_MASK_A|INPUT_MASK_B|INPUT_MASK_C|INPUT_MASK_START)
 
 #define PMASK0 (INPUT_MASK_UP)
@@ -30,6 +34,11 @@ char bgColor = 0;
 
 char dx = 1, dy = 1;
 char dxA = -2, dyA = 2;
+char paddleX = 64;
+#define BUTTONTESTPOSY 7
+#define BINARYTESTPOSY 10
+char button_byte=0;
+
 void soundTest(){
     //play_sound_effect(ASSET__audio__hit_bin_ID,1);
     play_sound_effect(ASSET__audio__flongNew_sfx_ID,(char)1);
@@ -98,9 +107,18 @@ char buttons_to_byte(int player1_buttons) {
     
     return result;
 }
-#define BUTTONTESTPOSY 7
-#define BINARYTESTPOSY 10
 
+
+void paddleDraw(char potVal)
+{
+    char paddlex=potVal;
+    if (paddlex < 1){
+        paddlex = 1;
+    } else if (paddlex>127-PADDLEWIDTH){
+        paddlex = 127 - PADDLEWIDTH;
+    }
+    queue_draw_box(paddlex,PADDLEY,PADDLEWIDTH,PADDLEHEIGHT,PADDLECOLOR);
+}
 void inputButtonsDraw()
 {
     //input testing
@@ -122,15 +140,12 @@ void inputBinaryDraw()
     //
 
     //try to convert inputs detected into a byte, then that byte should easily translate to a pixel position
-    char button_byte;
-    button_byte = buttons_to_byte(player1_buttons);
     
     //if (player1_buttons==0){queue_draw_box(64,64,1,1,30);}
     //unsigned int x_position = button_byte;  // Automatically converts to unsigned int
     queue_draw_box(0,BINARYTESTPOSY,button_byte,1,182);
 
 }
-
 
 void main () {
     init_music();
@@ -141,15 +156,15 @@ void main () {
         queue_clear_screen(256);//256 black
         queue_draw_box(box_x, box_y, 8, 8, BOXCOLOR);
         queue_draw_box(boxA_x, boxA_y, 8, 8, BOXCOLORA);
+        button_byte = buttons_to_byte(player1_buttons);
 
         inputButtonsDraw();
-
         inputBinaryDraw();//interpret button presses as 7 byte number
 
         queue_clear_border(2);
         boxMotion();
         boxAMotion();
-
+        paddleDraw(button_byte);
         await_draw_queue();
         await_vsync(1);
         flip_pages();
