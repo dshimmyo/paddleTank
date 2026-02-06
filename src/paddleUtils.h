d420009cce842c686caccc1f05873b67adcb518f
Daniel Shimmyo <dshimmyo@gmail.com>
2026-02-06 13:16:43 -0500

    collision fixes and moved defines to separate file

--- src/paddleUtils.h
+++ src/paddleUtils.h
@@ -7,5 +7,22 @@
 #define OCEAN 0b11011111
 #define ALGAE 0b11111111
 #define WHITE 0b00000111
-
 unsigned char packColor(unsigned char h,unsigned char s, unsigned char v) ;
+//new masks for experimental gamepad hardware:
+//assignments based on genesis controller pinout
+#define INPUT_MASK_MODE 1//first read 00000001
+#define INPUT_MASK_X 2 //first read 00000010
+#define INPUT_MASK_Y 4 //need to add another multiplexor or build npn/pnp circuit
+#define INPUT_MASK_Z 8 //need to add another multiplexor or build npn/pnp circuit
+#define PMASK0 (INPUT_MASK_UP) //should be second read pin 1
+#define PMASK1 (INPUT_MASK_DOWN)
+#define PMASK2 (INPUT_MASK_LEFT)
+#define PMASK3 (INPUT_MASK_RIGHT)
+#define PMASK4 (INPUT_MASK_A)
+#define PMASK5 (INPUT_MASK_B)
+#define PMASK6 (INPUT_MASK_C)
+#define PMASK7 (INPUT_MASK_START)
+#define PMASK8 (INPUT_MASK_X) //EXPERIMENTAL GAME HARDWARE
+#define PMASK9 (INPUT_MASK_Y) //EXPERIMENTAL GAME HARDWARE
+#define PMASK10 (INPUT_MASK_Z) //EXPERIMENTAL GAME HARDWARE
+#define PMASK11 (INPUT_MASK_MODE) //EXPERIMENTAL GAME HARDWARE
