a6460aef8293a43303fa035fa6998adeb4973578
Daniel Shimmyo <dshimmyo@gmail.com>
2026-02-15 08:07:38 -0500

    added custom number formatting with leading zeros, and trying a new function to convert num to string but it's still slow

--- src/scoring.c
+++ src/scoring.c
@@ -1,6 +1,35 @@
 #include "gt/feature/text/text.h"
 #include <stdio.h>
+#include <stdint.h>
 
+void uint_to_string(unsigned int num, char* buffer) {
+    char* ptr = buffer;
+    char* start;
+    char* end;
+    
+    // Handle the special case for 0
+    if (num == 0) {
+        *ptr++ = '0';
+        *ptr = '\0';
+        return;
+    }
+
+    // Convert number to string in reverse
+    while (num > 0) {
+        *ptr++ = '0' + (num % 10);
+        num /= 10;
+    }
+    *ptr = '\0'; // Null terminate the string
+
+    // Reverse the string to correct order
+    start = buffer;
+    end = ptr - 1;
+    while (start < end) {
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+}
 
 void print_scores(int num)
 {
@@ -9,8 +38,16 @@ void print_scores(int num)
     text_color = 0;
     text_cursor_y = 10;
     text_cursor_x = 5;
-    sprintf(buffer, "%d", num);
-    text_print_string(buffer);
+    if (num > 9999) text_print_string("9999");
+    else
+    {
+        if (num < 10) text_print_string("000");
+        else if (num < 100) text_print_string("00");
+        else if (num < 1000) text_print_string("0");
+        //sprintf(buffer, "%d", num);
+        uint_to_string(num,buffer);
+        text_print_string(buffer);
+    }
 }
 void scoring_init()
 {
