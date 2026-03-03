835990682465bc2883865afc9438adf3f0356086
Daniel Shimmyo <dshimmyo@gmail.com>
2026-03-03 12:37:35 -0500

    soundhack cleanup, add notes

--- extras/soundhack.pl
+++ extras/soundhack.pl
@@ -6,6 +6,8 @@ use Cwd qw(chdir);
 #chdir("../assets/audio/");
 my $framelength = @ARGV[0];#max 32 "20"in hex
 my $frequency = @ARGV[1];# min 8 max 107 middleC C4 = 48
+#my $amplitude = @ARGV[2]; #min 0 max 8 currently set to 6 or 7, 
+
 my $outfile = @ARGV[2]; #"../assets/audio/perlTest.sfx";
 
 if (!@ARGV) {
@@ -27,47 +29,25 @@ if (!@ARGV) {
 }
 else
 {
-    # $framelength = @ARGV[0];
     if ($framelength > 32) {$framelength = 32;};
     print ("framelength set to ". $framelength ."\n");
 };
-#
-#$test = @ARGV[0];
-#text file test
-my $output = "0800 0007 0606 2a2a 2a2a 0007 0606 2a2a
-2a2a 0007 0606 2a2a 2a2a 0007 0606 2a2a
-2a2a 0007 0606 2a2a 2a2a 0007 0606 2a2a
-2a2a 0007 0606 2a2a 2a2a 0007 0606 1010
-1010";
-
-
-# open(my $filehandle, '>', $outfile) or die "Could not open file: $!";
-# print $filehandle $output;
-# close $filehandle;
 
 #binary test
 open(my $out, '>:raw', $outfile) or die "Unable to open: $!";
-#print $out pack('s<', 255 );
 my $hex = sprintf("0x%X", $framelength);
-#my $firstbyte = ($hex."00");
 print $out pack('s<', hex($hex) );
-my $hexFreq = sprintf("0x%X", ($frequency * 257));
-#my $freqByte = ($hexFreq.$hexFreq);
-
-#print $out pack('s>', hex($hex));
+my $hexFreq = sprintf("0x%X", ($frequency * 257)); #mult 257 to turn two bytes into four bytes
 
 for ($i=0; $i<$framelength;$i++){
-    print $out pack('s>', hex("0007") );#Amp 0,1
-    print $out pack('s>', hex("0606") );#Amp 2,3
+    print $out pack('s>', hex("0007") );#Amp 0,1 ##first opamp sound like a modulator or boost, second seem like soft to hard tone
+    print $out pack('s>', hex("0606") );#Amp 2,3 # keep 6,6
     # print $out pack('s>', hex("2a2a") );#freq 0,1
     # print $out pack('s>', hex("2a2a") );#freq 0,1
     print $out pack('s<', hex($hexFreq) );
     print $out pack('s<', hex($hexFreq) );
 
 };
-# print $out pack('s<', 39);
-# print $out pack('s<', 255);
-# print $out pack('s<', 39);
 close($out);
 
 
