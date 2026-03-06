e45cf68c88364d0a44aee1533e602bc0e9855be1
Daniel Shimmyo <dshimmyo@gmail.com>
2026-03-06 10:13:58 -0500

    refresh script

---
+++ extras/refreshSounds.sh
@@ -0,0 +1,9 @@
+./soundhack.pl 5 72 ../assets/audio/wall.sfx   
+./soundhack.pl 5 58 ../assets/audio/row0.sfx   
+./soundhack.pl 5 55 ../assets/audio/row1.sfx   
+./soundhack.pl 5 51 ../assets/audio/row2.sfx   
+./soundhack.pl 5 46 ../assets/audio/row3.sfx   
+./soundhack.pl 5 42 ../assets/audio/row4.sfx   
+./soundhack.pl 5 62 ../assets/audio/ceiling.sfx
+./soundhack.pl 5 62 ../assets/audio/paddle.sfx 
+
